bb99585746eb580f9bf5ee06c95495b54377461a
Brian <brian.salkas@gmail.com>
2023-03-03 13:36:10 -0600

    add contribute sidebar

--- spellingbee_solver.py
+++ spellingbee_solver.py
@@ -1,8 +1,15 @@
 import solve
 import datetime
 import streamlit as st
-# if clicked solve!
-st.title("Get the words to todays NYT spelling bee")
-if st.button(f"get answers for spelling bee {datetime.date.today()}"):
-    st.write("solving the spelling bee")
-    solve.solve()
+
+
+if __name__ == "__main__":
+    st.title("Get the words to todays NYT spelling bee")
+    if st.button(f"get answers for spelling bee {datetime.date.today()}"):
+        st.write("solving the spelling bee")
+        solve.solve()
+    with st.sidebar:
+        st.title("created by Brian Salkas")
+        st.header("Contribute")
+        st.write("missing a word and/or contains invalid word?")
+        st.write("open an issue or submit a pull request [here](https://github.com/brianSalk/spelling-bee-solver-frontend)")
