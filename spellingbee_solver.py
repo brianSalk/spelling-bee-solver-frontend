9eea37fdac38d74581d422e0ee539b272ae8d030
Brian <brian.salkas@gmail.com>
2023-03-03 09:00:10 -0600

    add date to button

--- spellingbee_solver.py
+++ spellingbee_solver.py
@@ -1,7 +1,8 @@
 import solve
+import datetime
 import streamlit as st
 # if clicked solve!
 st.title("Get the words to todays NYT spelling bee")
-if st.button("Solve todays spelling bee!"):
+if st.button(f"get answers for spelling bee {datetime.date.today()}"):
     st.write("solving the spelling bee")
     solve.solve()
