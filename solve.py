cba53cf54e806cd2d600a748d63b19aa1d56efb7
Brian Salkas <brian.salkas@gmail.com>
2024-08-28 00:12:21 -0500

    Remove unused code

--- solve.py
+++ solve.py
@@ -1,7 +1,6 @@
 import selenium
 import time
 import sys
-import argparse
 import word_proc
 import streamlit as st
 import format_words
@@ -10,39 +9,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-# argparse stuff
-parser = argparse.ArgumentParser()
-parser.add_argument("--browser", help="Browser used to solve spellingbee, default is firefox", default='firefox')
-parser.add_argument("-l","--login", action='store_true', help='use to manually log in to NYT')
-parser.add_argument("-v", "--verbose", action="store_true", help='print words that are being tried')
-parser.add_argument("-S","--no_solve",action="store_true", help='do not solve, only list words in terminal')
-args = parser.parse_args()
 
-def create_driver(browser):
-    if browser == 'firefox':
-        driver = selenium.webdriver.Firefox()
-    elif browser == 'chrome':
-        driver = selenium.webdriver.Chrome()
-    elif browser == 'edge':
-        driver = selenium.webdriver.Edge()
-    else:
-        print(f'unsupported browser called: {args.browser}')
-        print(f'contact me at github or open an issue to get {args.browser} support')
-        sys.exit(1)
-    return driver
 
 def open_login_page(driver):
     driver.get('https://myaccount.nytimes.com/auth/enter-email?redirect_uri=https%3A%2F%2Fwww.nytimes.com%2Fpuzzles%2Fspelling-bee&amp;response_type=cookie&amp;client_id=games&amp;application=crosswords&amp;asset=navigation-bar')
     return driver 
 def solve():
-    from selenium.webdriver.chrome.options import Options
-    # setup chrome options 
-    options = Options()
-    options.add_argument('--no-sandbox')
-    options.add_argument('--headless')
     driver = None
     try:
-        driver = get_chrome_webdriver() # switch back to Chrome
+        driver = get_chrome_webdriver()
         url = 'https://www.nytimes.com/puzzles/spelling-bee'
         driver.get(url)
         time.sleep(2)
